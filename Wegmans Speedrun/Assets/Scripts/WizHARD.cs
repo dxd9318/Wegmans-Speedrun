320c6dc53274b47b5d8ccc8ee81696780429a3db
kat weis <kxw4589@g.rit.edu>
2018-02-11 03:37:40 -0500

    setup basic health system and canvas for battle

---
+++ Wegmans Speedrun/Assets/Scripts/WizHARD.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AttackSpell
+{
+    Fireball
+}
+
+public enum DefendSpell
+{
+    Counter,
+    Block,
+    Heal
+}
+
+public class WizHARD : MonoBehaviour {
+
+    public List<DefendSpell> defSpellPool;//list of defensive spells the ai wizard can pull from
+    public List<AttackSpell> atkSpellPool;//list of offensive spells the ai wizard can pull from
+
+    private bool myTurn;//whether it is the wizard's turn or not
+
+    // Use this for initialization
+    void Start () {
+        myTurn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myTurn)
+        {
+            Attack();
+            myTurn = false;
+        }
+	}
+
+    private void Defend()
+    {
+
+    }
+
+    private void Attack()
+    {
+        int spellIndex = Random.Range(0, atkSpellPool.Count - 1);
+        GameObject.Find("Player").GetComponent<Health>().TakeDamage(10);
+        Debug.Log("he attac");
+    }
+
+    private void CastSpell()
+    {
+
+    }
+}
